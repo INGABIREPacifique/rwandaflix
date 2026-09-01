6103cdff64033a21d8fbf6c0615d36203a414c07
INGABIREPacifique <pacifiqueingabire222@gmail.com>
2026-09-01 11:14:00 +0200

    Restore core RwandaFlix styling in production app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode, createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './App.css'
 import './responsive.css'
 import App from './App.jsx'
 import AppErrorBoundary from './components/AppErrorBoundary.jsx'
