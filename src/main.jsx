67f49ee6edd8adbce34f32e4eae9349d576d9088
INGABIREPacifique <pacifiqueingabire222@gmail.com>
2026-09-01 12:07:16 +0200

    Stabilize RwandaFlix application entry and preserve backend bridge

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode, createElement } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
-import './responsive.css'
 import App from './App.jsx'
 import AppErrorBoundary from './components/AppErrorBoundary.jsx'
 import RwandaFlixBackendBridge from './components/RwandaFlixBackendBridge.jsx'
@@ -17,5 +16,5 @@ function Root() {
 }
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>{createElement(Root)}</StrictMode>
+  <StrictMode><Root /></StrictMode>
 )
